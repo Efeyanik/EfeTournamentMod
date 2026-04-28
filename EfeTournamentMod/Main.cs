3ab5bde373cd55cdb09b016ed0d73ea7b20aa5ea
Efe <efeynkoff@hotmail.com>
2026-04-28 21:36:35 +0300

    Proje dosyası ekle.

---
+++ EfeTournamentMod/Main.cs
@@ -0,0 +1,28 @@
+﻿using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace EfeTournamentMod
+{
+    public class Main : MBSubModuleBase
+    {
+        // 1. Aşama: Mod yüklenirken (arkaplanda) Harmony'i başlatıyoruz. Burası en güvenli yer.
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            Harmony harmony = new Harmony("com.efeyanik.tournamentmod");
+            harmony.PatchAll();
+        }
+
+        // 2. Aşama: Ana menü ekranı yüklenip karşımıza gelmeden hemen önceki an.
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+            // UI artık hazır olduğu için mesajımızı buraya koyuyoruz.
+            InformationManager.DisplayMessage(new InformationMessage("Efe'nin Turnuva Modu basariyla yuklendi!", Colors.Green));
+        }
+    }
+}
