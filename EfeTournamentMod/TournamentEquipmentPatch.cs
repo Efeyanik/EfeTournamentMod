d40d59001c1f7c4af6fc344679db6d7ea2883814
Efe <efeynkoff@hotmail.com>
2026-05-06 00:01:02 +0300

    TournamentMenuPatch.cs and horse inquiry added. Now we ask the player whether they will be participating with their own equipment or tournament equipment. We add the horse depending on whether there is a horse in that match or not.

--- EfeTournamentMod/TournamentEquipmentPatch.cs
+++ EfeTournamentMod/TournamentEquipmentPatch.cs
@@ -6,32 +6,53 @@ using TaleWorlds.Core;
 
 namespace EfeTournamentMod
 {
-    
+   
     [HarmonyPatch(typeof(DefaultTournamentModel), "GetParticipantArmor")]
     public class TournamentArmorPatch
     {
-        
         public static void Postfix(CharacterObject participant, ref Equipment __result)
         {
-            if (participant != null && participant.IsPlayerCharacter)
+            if (participant != null && participant.IsPlayerCharacter && ModSettings.KendiEsyamlaGir)
             {
-                // Turnuva zırhı yerine kend karakterimizin ekipmanını veriyoruz.
-                __result = participant.HeroObject.BattleEquipment;
+                // Oyunun bu maç için tasarladığı orijinal zırh paketinde at var mı?
+                bool atOlsunMu = __result[EquipmentIndex.Horse].Item != null;
+
+                // 'new Equipment()' ile KLON yaratıyoruz
+                Equipment gercekEkipman = new Equipment(participant.HeroObject.BattleEquipment);
+
+                // Eğer oyun bu maçta at istemiyorsa (yaya dövüşüyse), klondan atı sil
+                if (!atOlsunMu)
+                {
+                    gercekEkipman[EquipmentIndex.Horse] = EquipmentElement.Invalid;
+                    gercekEkipman[EquipmentIndex.HorseHarness] = EquipmentElement.Invalid;
+                }
+
+                __result = gercekEkipman;
             }
         }
     }
 
-    
     [HarmonyPatch(typeof(TournamentParticipant), "set_MatchEquipment")]
     public class TournamentWeaponPatch
     {
-        
         public static void Prefix(TournamentParticipant __instance, ref Equipment value)
         {
-            if (__instance.Character != null && __instance.Character.IsPlayerCharacter)
+            if (__instance.Character != null && __instance.Character.IsPlayerCharacter && ModSettings.KendiEsyamlaGir)
             {
-                //kendi gerçek savaş ekipmanımızı veriyoruz.
-                value = __instance.Character.HeroObject.BattleEquipment;
+                // Oyunun bu el için vereceği silahlarda at var mı?
+                bool atOlsunMu = value[EquipmentIndex.Horse].Item != null;
+
+                // Gerçek savaş eşyalarımızın klonunu alıyoruz
+                Equipment gercekEkipman = new Equipment(__instance.Character.HeroObject.BattleEquipment);
+
+                // Eğer maç yaya maçıysa atı ve eyerini (HorseHarness) yolluyoruz
+                if (!atOlsunMu)
+                {
+                    gercekEkipman[EquipmentIndex.Horse] = EquipmentElement.Invalid;
+                    gercekEkipman[EquipmentIndex.HorseHarness] = EquipmentElement.Invalid;
+                }
+
+                value = gercekEkipman;
             }
         }
     }
---
+++ EfeTournamentMod/TournamentMenuPatch.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameMenus; 
+using TaleWorlds.CampaignSystem.TournamentGames;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace EfeTournamentMod
+{
+    // Bütün mod boyunca diğer sınıfların da okuyabileceği global bir flag .
+    public static class ModSettings
+    {
+        // Varsayılan olarak false (Yani geleneksel tahta silahlar)
+        public static bool KendiEsyamlaGir = false;
+    }
+
+    [HarmonyPatch(typeof(TournamentCampaignBehavior), "game_menu_tournament_join_current_game_on_consequence")]
+    public class TournamentMenuPatch
+    {
+        
+        public static bool Prefix()
+        {
+            
+            InquiryData inquiryData = new InquiryData(
+                "Tournament Equipment", // Başlık
+                "What equipment would you like to bring to the arena?",
+                true,
+                true, 
+                "My Own Equipment", 
+                "Tournament Equipments", 
+
+                
+                () =>
+                {
+                    ModSettings.KendiEsyamlaGir = true; 
+                    GirmeyeDevamEt(); 
+                },
+
+                //(Negative Action)
+                () =>
+                {
+                    ModSettings.KendiEsyamlaGir = false; 
+                    GirmeyeDevamEt(); 
+                }
+            );
+
+            
+            InformationManager.ShowInquiry(inquiryData, false);
+
+            
+            return false;
+        }
+
+        
+       
+        private static void GirmeyeDevamEt()
+        {
+            TournamentGame tournamentGame = Campaign.Current.TournamentManager.GetTournamentGame(Settlement.CurrentSettlement.Town);
+            GameMenu.SwitchToMenu("town");
+            tournamentGame.PrepareForTournamentGame(true);
+            Campaign.Current.TournamentManager.OnPlayerJoinTournament(tournamentGame.GetType(), Settlement.CurrentSettlement);
+        }
+    }
+}
